9f2bf4ec685a56fe4f31b8afe09ee0146e585cca
tigakub <tigakub@mac.com>
2021-06-23 05:02:42 -0700

    Successfully getting IMU telemetry

---
+++ build/CMakeFiles/realsense_test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tigakub/Documents/RealSense/Texture.cpp" "CMakeFiles/realsense_test.dir/Texture.cpp.o" "gcc" "CMakeFiles/realsense_test.dir/Texture.cpp.o.d"
+  "/home/tigakub/Documents/RealSense/Window.cpp" "CMakeFiles/realsense_test.dir/Window.cpp.o" "gcc" "CMakeFiles/realsense_test.dir/Window.cpp.o.d"
+  "/home/tigakub/Documents/RealSense/main.cpp" "CMakeFiles/realsense_test.dir/main.cpp.o" "gcc" "CMakeFiles/realsense_test.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/realsense_test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/realsense_test.dir/Texture.cpp.o"
+  "CMakeFiles/realsense_test.dir/Texture.cpp.o.d"
+  "CMakeFiles/realsense_test.dir/Window.cpp.o"
+  "CMakeFiles/realsense_test.dir/Window.cpp.o.d"
+  "CMakeFiles/realsense_test.dir/main.cpp.o"
+  "CMakeFiles/realsense_test.dir/main.cpp.o.d"
+  "realsense_test"
+  "realsense_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/realsense_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
